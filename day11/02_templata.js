3d437ecbc83eecc755495cc7b519e29dc11a8d64
JiangChuang <2952830597@qq.com>
2018-09-30 16:16:13 +0800

    day11

---
+++ day11/02_templata.js
@@ -0,0 +1,12 @@
+//引入template模板
+// <script id="tpl" type="text/html">
+// <ul>
+//     <% for(var i = 0; i < list.length;i++){ %>
+//     <li><%= list[i].name %></li>
+//     <%}%>
+// </ul>
+// </script>
+
+// var tpl = document.getElementById('tpl').innerHTML;
+// template(tpl, {list: [{name: "yan"},{name: "haijing"}]});
+// <script id="test" type="text/html">
