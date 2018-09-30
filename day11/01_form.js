59975820b11ab8968991d5bc1afbd6b9fd4a02ed
JiangChuang <2952830597@qq.com>
2018-09-30 12:42:33 +0800

    day11

--- day11/01_form.js
+++ day11/01_form.js
@@ -1,6 +1,7 @@
 var fd = require('formidable');
 var http = require("http");
 var fs = require('fs');
+var sd = require("silly-datetime")
 var server = http.createServer(function (req, res) {
   res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
   if (req.url == '/favicon.ico') {
@@ -21,6 +22,9 @@ var server = http.createServer(function (req, res) {
      //使用formidable模块处理请求
      //创建表单对象
      var form = new fd.IncomingForm();
+    
+      //设置上传文件保存的路径
+      form.uploadDir = "./uploads";
 
      //解析请求对象
      //err:解析出错的错误信息
@@ -36,8 +40,28 @@ var server = http.createServer(function (req, res) {
       console.log(fields);
       console.log("=======files=========");
       console.log(files);
-      res.end("请求成功");
-    });
+       //对文件改名
+      // 获取旧路径
+      var oldPath = files.pic.path;
+      // 设置新路径
+      var oldName = files.pic.name;//获取名称
+      var arr = oldName.split(".");
+      var ext = arr[arr.length-1];//获取后缀名
+      //设置名称
+      var name = sd.format(new Date(),"YYYYMMDDHHmmss");
+      //上传文件的新名称
+      var newName = name+'.'+ext;
+      //新路径  
+      var newPath = './uploads/'+newName;
+      fs.rename(oldPath,newPath,function(err){
+        if(err){
+          console.log(err);
+          res.end('重命名失败');
+          return
+        }
+        res.end(newPath);
+      });
+    })
   }else{
     res.end("地址错误");
   }
